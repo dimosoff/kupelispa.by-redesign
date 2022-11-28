bccc40a7c003f78f1cfd16ecd539c57e787a83e4
dimosoff <dimmiostr@mail.ru>
2022-11-28 17:01:08 +0300

    some fixes

--- assets/minifyx/js/redesign.js
+++ assets/minifyx/js/redesign.js
@@ -1,15 +1,19 @@
 const searchButton = document.querySelector(".header-search-button");
 const searchElement = document.querySelector(".header-search");
 const topline = document.querySelector(".topline");
+const searchInput = document.getElementById("search");
 
 if (topline) {
-  addClassOnScroll('body', topline.clientHeight, 'scrolled');
+  addClassOnScroll("body", topline.clientHeight, "scrolled");
 }
 
 if (searchButton && searchElement) {
   const searchClose = document.querySelector(".search-close");
-  searchButton.addEventListener("click", function () {
+  searchButton.addEventListener("click", function (e) {
+    e.preventDefault();
     searchElement.classList.toggle("active");
+    if (!searchElement.classList.contains("active")) return;
+    searchInput.click();
   });
   searchClose.addEventListener("click", function () {
     searchElement.classList.remove("active");
